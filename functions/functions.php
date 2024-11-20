f4f9361e9cc58801cad980a2baaa78e3137e0bc7
JOSS <jo0zue99@hotmail.com>
2024-11-20 00:45:57 -0600

    update-dev: Estructura para las APIs y reporte de producto procesado

--- functions/functions.php
+++ functions/functions.php
@@ -1,4 +1,9 @@
 <?php
+# Variable de entorno
+define('URL', '/APIS_jwt/');
+
+
+# Funcion para validar el token obtenido con el entorno JWT
 function validaToken($token) {
     # Verifica que el token no esté vacío
     if (empty($token)) {
@@ -52,5 +57,29 @@ function validaToken($token) {
     http_response_code(200);
     echo json_encode(['success' => 'Token válido']);
     return true;
+}
+
+# Funcion para obtener los resultados de las querys 
+function obtenerResultados($query_psg, $pdo){
+    try {
+        // Prepara la consulta postgresql
+        $stmt = $pdo->prepare($query_psg);
+        // Ejecuta la consulta
+        $stmt->execute(); 
+        
+        // Obtenemos los resultados como un arreglo asociativo
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        // Si se obtienen resultados, devolverlos como JSON
+        if ($results) {
+            echo json_encode($results);
+        } else {
+            // Si no hay resultados, devolvera un error en formato JSON
+            echo json_encode(["error" => "No se encontraron resultados."]);
+        }
+    } catch (PDOException $e) {
+        // Si ocurre un error, devolvera el error en formato JSON
+        echo json_encode(["error" => "Error en la consulta: " . $e->getMessage()]);
+    }
 }
 ?>
